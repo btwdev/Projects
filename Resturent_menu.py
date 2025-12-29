edcd7232fedef4373b9e64e2153950c663501194
devshar9496-arch <devshar9496@gmail.com>
2025-12-29 19:04:13 +0530

    Project

--- Resturent_menu.py
+++ Resturent_menu.py
@@ -1,9 +1,11 @@
-print("--- MENU ---\n1. pizza   ₹100\n2. Burger    ₹50\n3. Maggie   ₹100\n4. Chai   ₹10\n~Please Comform your Order\n")
+print("--- MENU ---\n1. pizza   ₹100\n2. Burger    ₹50\n3. Maggie   ₹100\n4. Chai   ₹10\n5. Ice Creane    ₹70\n6.Break Samosa    ₹30\n~Please Comform your Order\n")
 user = input("Place Your First Order:").strip().lower()
 Pricing = {"pizza" : 100,
         "Burger" : 50,
         "Maggie" : 100,
-        "Chai" : 10}
+        "Chai" : 10,
+        "IceCreame" : 70,
+        "BreakSamosa": 30}
 order = []
 if user=="1":
     order.append(Pricing["pizza"])
@@ -17,12 +19,20 @@ elif user=="3":
 elif user=="4":
     order.append(Pricing["Chai"])
     print("You Order Chai")
+elif user=="5":
+    order.append(Pricing["IceCreame"])
+    print("You Order Ice Creame")
+elif user=="6":
+    order.append(Pricing["BreakSamosa"])
+    print("You Order BreakSamosa")
 
 while True:
     user = input("Place Your Next Order & 'no' For View Bill:").strip().lower()
     if user=="no":
         print(f"Your Total Bill : {sum(order)}")
         print("Thank You For Visiting")
+        print("You Have a Good Day")
+        break
     elif user=="1":
         order.append(Pricing["pizza"])
         print("You Order Pizza")
@@ -35,5 +45,11 @@ while True:
     elif user=="4":
         order.append(Pricing["Chai"])
         print("You Order Chai")
+    elif user=="5":
+        order.append(Pricing["IceCreame"])
+        print("You Order Ice Creame")
+    elif user=="6":
+        order.append(Pricing["BreakSamosa"])
+        print("You Order BreakSamosa")
     else:
         print("Please Pick The correct Option")
